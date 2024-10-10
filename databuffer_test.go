9601b45b7fc134ca7c16262ab1045dc8ee383afd
Scott Lampert <scott@lampert.org>
2024-10-10 16:12:42 -0400

    Add slice test

--- databuffer_test.go
+++ databuffer_test.go
@@ -71,6 +71,44 @@ func TestDataBuffer(t *testing.T) {
 	require.Equal(t, numStrings, len(reporter.GetResults()))
 }
 
+func TestDataBufferSlices(t *testing.T) {
+	const (
+		numStrings     = 121
+		numGenerations = 11
+	)
+
+	reporter := &RecorderReporter{}
+
+	opts := databuffer.Options[string]{
+		NumWorkers:    4,
+		MaxBufferSize: 512,
+		WorkerWait:    3 * time.Second,
+		Reporter:      reporter,
+	}
+
+	dbuf, err := databuffer.New(opts)
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	dbuf.Start(ctx)
+
+	go func() {
+		for range numGenerations {
+			dbuf.WorkerChan() <- GenerateRandomStrings(numStrings, 8)
+			n := rand.Int64N(5)
+			time.Sleep(time.Duration(n) * time.Millisecond)
+		}
+		cancel()
+	}()
+
+	<-ctx.Done()
+	time.Sleep(1 * time.Second)
+
+	require.Equal(t, numStrings*numGenerations, len(reporter.GetResults()))
+}
+
 func GenerateRandomStrings(num int, length int) []string {
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	seededRand := rand.New(rand.NewPCG(1, 2))
